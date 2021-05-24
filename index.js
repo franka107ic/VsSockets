a5f2da19c2eafabf1522706ca1733c94dab16350
Frank Carry <fcaryv@gmail.com>
2021-05-24 15:25:09 -0500

    [IC-9} Auth pending completed

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ const app = express();
 const { Server } = require("socket.io");
 const https = require("https");
 const fs = require("fs");
-// const privateKey = fs.readFileSync(
-//   "/etc/letsencrypt/live/ic-proj-updates-sockets-server.ideascloud.io/privkey.pem",
-//   "utf8"
-// );
-// const certificate = fs.readFileSync(
-//   "/etc/letsencrypt/live/ic-proj-updates-sockets-server.ideascloud.io/fullchain.pem",
-//   "utf8"
-// );
-const privateKey = fs.readFileSync("./privkey.pem");
-const certificate = fs.readFileSync("./fullchain.pem");
+const ProjectsManager = require("ideascloud-sdk/classes/ProjectsManager");
+
+const privateKey = fs.readFileSync(
+  "/etc/letsencrypt/live/ic-proj-updates-sockets-server.ideascloud.io/privkey.pem",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  "/etc/letsencrypt/live/ic-proj-updates-sockets-server.ideascloud.io/fullchain.pem",
+  "utf8"
+);
+
+// const privateKey = fs.readFileSync("./privkey.pem");
+// const certificate = fs.readFileSync("./fullchain.pem");
 const credentials = { key: privateKey, cert: certificate };
 const httpsServer = https.createServer(credentials, app);
 const io = new Server(httpsServer, {
@@ -28,24 +31,22 @@ app.use("/", (req, res) => {
   res.send("<h1>Welcome to ideascloud socket server</h1>");
 });
 
-app.post("/emitChange", (req, res) => {
-  io.broadcast.to(req.body.projectId);
-});
-
-io.use((socket, next) => {
+io.use(async (socket, next) => {
   console.log(socket.handshake.query);
   if (socket.handshake.query && socket.handshake.query.token) {
-    //TODO: Verify from ideascloud-sdk
-    //------------- Test Auth -------------------
-    const tokens = ["token1", "token2"];
-    if (tokens.find((token) => token === socket.handshake.query.token)) {
+    //TODO: [IC-9] Verify from ideascloud-sdk
+    try {
+      const projectsManager = new ProjectsManager({
+        accessToken: socket.handshake.query.token,
+      });
+      await projectsManager.listProjects();
+      console.log("Auth succesfully");
       next();
-    } else {
-      throw new Error("Authentication Error");
+    } catch (e) {
+      console.error("Authenticaton error");
     }
-    //-------------------------------------------
   } else {
-    throw new Error("Authentication Error");
+    console.error("Authentication Error");
   }
 });
 
